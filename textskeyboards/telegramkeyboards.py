61c39b0699ee9e7dfce8ca8fe68d8745195b5d0f
Nikolai <kirichk@gmail.com>
2021-06-27 18:02:09 +0300

    fix

---
+++ textskeyboards/telegramkeyboards.py
@@ -0,0 +1,30 @@
+menu_keyboard = ('Запис на відеоконференцію',
+                 "Зв'язок з оператором")
+
+continue_keyboard = ('Продовжити',
+                     'Меню')
+
+phone_keyboard = ('Подiлитись номером',
+                  "Зв'язок з оператором")
+
+category_keyboard = (['Пральні машини', 'category-Stiralki'],
+                     ['Пральні машини з сушкою', 'category-Stiralki_Sushilki'],
+                     ['Посудомийні машини', 'category-Posudomoyki'],
+                     ['Сушильні машини', 'category-Sushilki'],
+                     ['Холодильники', 'category-Holodilnik'],
+                     ['Морозильні камери', 'category-Morozilnik'],
+                     ['Плити з духовою шафою', 'category-Plity_Duhovye_Shkafi'],
+                     ['Духові шафи', 'category-Duhovye_Shkafi'],
+                     ['Варильні поверхні', 'category-Varochi'],
+                     ['Мікрохвильові печі', 'category-Mikrovolnovki'],
+                     ['Пилососи', 'category-Pylesosy'],
+                     ['Інше', 'category-Drugoe'])
+
+brand_keyboard = ('Candy',
+                  'Hoover',
+                  'Rosieres')
+
+photo_keyboard = ("Зв'язок з оператором",
+                  'Продовжити')
+
+back_to_date = ("Вибрати iншу дату")
